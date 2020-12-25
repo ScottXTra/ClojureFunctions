f68036f47f95fbc95e8791b21c21be6bfeafa29c
Scott <sweselan@uoguelph.com>
2020-12-24 22:10:35 -0500

    added exponant calc and taylorSerise sin

--- math.clj
+++ math.clj
@@ -1,2 +1,10 @@
+
+
 (defn circ [radius]
     (* radius 2 3.14159))
+
+(defn exp [x n]
+  (reduce * (repeat n x)))
+
+(defn taylorSin [radian]
+    (+(-radian(/(exp radian 3)6))(/(exp radian 5) 120)))  
