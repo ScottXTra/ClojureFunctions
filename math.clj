c77b21f6dba52b8010d64d7437e510f6a571f86e
Scott <sweselan@uoguelph.com>
2020-12-25 23:38:35 -0500

    added cube and sqaure calc

--- math.clj
+++ math.clj
@@ -10,5 +10,21 @@
 
 ; Should be accurate from 0 -> PI
 (defn taylorSin [radian]
-   (format "%.5f" (- (+ (- (+ (- radian (/ (exp radian 3) 6)) (/ (exp radian 5) 120)) (/ (exp radian 7) 5040)) (/ (exp radian 9) 362880) )  (/ (exp radian 11) 39916800) )))
+   (format "%.3f" (- (+ (- (+ (- radian (/ (exp radian 3) 6)) (/ (exp radian 5) 120)) (/ (exp radian 7) 5040)) (/ (exp radian 9) 362880) )  (/ (exp radian 11) 39916800) )))
 
+; Quadratic formula (Format: ax^2 + bx + c)
+(defn quadRoots [a b c]
+  (let [
+        undersqrroot    (Math/sqrt (- (* b b) (* 4 a c)))
+        negb            (- b)
+        bottom          (/ 1 (* 2 a))
+        ]
+    [(* bottom (+ negb undersqrroot)) (* bottom (- negb undersqrroot))]))
+
+; Calculate volume of a cube with a given side legth
+(defn calcCubeVolume [sideLegth]
+    (* sideLegth sideLegth sideLegth))
+
+; Calculate area of a square with a given side legth
+(defn calcCubeVolume [sideLegth]
+    (* sideLegth sideLegth))
