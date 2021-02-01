3baa818765e5acee7b0bac1ddb1262c8c6477f47
Scott <sweselan@uoguelph.com>
2021-01-31 20:23:04 -0500

    Added tripleEveryElement function

--- sequences.clj
+++ sequences.clj
@@ -4,3 +4,7 @@
 (require '[clojure.string :as str])
 (defn countWordsInSentence [sentence]
     (count (str/split sentence #"\s+")))
+
+; returns a new list that is the input sequence with 1 added to every element
+(defn tripleEveryElement [sequence]
+     (map inc sequence))
