876747c5227b44ec3f7276f449b4e5bc39ed0b48
Scott <sweselan@uoguelph.com>
2021-03-01 18:09:40 -0500

    Added addDigits leetcode answer

--- lib1.clj
+++ lib1.clj
@@ -1,1 +1,13 @@
+;https://leetcode.com/problems/add-digits/
+(defn digits [n]
+  (->> n str (map (comp read-string str))))
+(defn addDigits [num]
+    (def dg (digits num))
+    (if (> (count dg) 1) (addDigits (reduce + (conj dg))) dg )
+)
+
+;https://leetcode.com/problems/add-digits/
+(defn addDigitsShort [num]
+    (if (= num 0) 0 (+ 1 (mod (- num 1) 9))))
+
 
